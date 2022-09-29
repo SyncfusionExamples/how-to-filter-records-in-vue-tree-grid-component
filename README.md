0d742f11662036bfe017c341d52e39d5539566d9
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2022-09-29 20:36:35 +0530

    Vue Tree Grid filtering sample
    Vue Tree Grid filtering sample

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# how-to-filter-records-in-vue-tree-grid-component
+# How to Filter Records in Vue Tree Grid Component
+
 A quick-start project that helps you filter records in the Vue Tree Grid component. This project contains code to enable filters with their different modes and types. It also has code to filter a tree grid records on initial loading.
+
+Refer to the following documentation on filtering in the Syncfusion Vue Tree Grid component: 
+https://ej2.syncfusion.com/vue/documentation/treegrid/filtering/filtering/
+
+Refer to the following documentation to learn about filter operators:
+https://ej2.syncfusion.com/vue/documentation/treegrid/filtering/filtering/#filter-operators
+
+Check out this online filtering example of the Syncfusion Vue Tree Grid component: 
+https://ej2.syncfusion.com/vue/demos/#/bootstrap5/tree-grid/filtering.html
+
+Refer to the following Syncfusion Vue Tree Grid getting started video: 
+https://www.youtube.com/watch?v=FEMyOHKjjao
+
+## Project prerequisites
+
+Make sure that you have the latest versions of Vue, Node, Vue Class Component, and Visual Studio Code in your machine before starting to work on this project.
+
+### How to run this application?
+
+To run this application, you need to clone the `how-to-filter-records-in-vue-tree-grid-component` repository and then open it in Visual Studio Code. Now, simply install all the necessary react packages into your current project using the `npm install` command and run your project using the `npm run serve` command.
---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <ejs-treegrid :dataSource="data"
+    childMapping="subtasks"
+    :treeColumnIndex="1"
+    :height='400' 
+    :allowFiltering="true"
+    :filterSettings="filterOptions"
+    :allowPaging='true' 
+    :pageSettings='pageSettings'>
+    <e-columns>
+      <e-column field='taskID' headerText='Task ID' width='80' textAlign='Right'></e-column>
+      <e-column field='taskName' headerText='Task Name' width='190'></e-column>
+      <e-column field='startDate' headerText='Start Date' width='100' format="yMd" textAlign='Right'></e-column>
+      <e-column field='duration' headerText='Duration' width='90' textAlign='Right'></e-column>
+      <e-column field='progress' headerText='Progress' width='90' textAlign='Right'></e-column>
+    </e-columns>
+  </ejs-treegrid>
+</template>
+
+<script>
+import { TreeGridComponent, ColumnsDirective, ColumnDirective, Page, Filter} from "@syncfusion/ej2-vue-treegrid";
+import {sampleData} from './data.js';
+
+export default{
+  name: 'App',
+  components: {
+    'ejs-treegrid': TreeGridComponent,
+    'e-columns': ColumnsDirective,
+    'e-column': ColumnDirective
+  },
+  data: () => {
+    return {
+      data: sampleData,
+      filterOptions: {
+        ignoreAccent:true, 
+        hierarchyMode:"None",
+        type: "Excel",
+        // Uncomment below code for initial filtering
+        // columns:[{
+        //   field: "taskName",
+        //   matchCase: false,
+        //   operator: "startswith",
+        //   predicate: "and",
+        //   value: "plan"
+        // }]
+      },
+      pageSettings: { pageSize: 12 }
+    }
+  },
+  provide:{
+    treegrid:[Page, Filter]
+  }
+}
+</script>
+
+<style>
+  @import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-calendars/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-dropdowns/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-popups/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-vue-grids/styles/material.css";
+  @import "../node_modules/@syncfusion/ej2-vue-treegrid/styles/material.css";
+</style>
